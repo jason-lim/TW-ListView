bd5f20b25a3ed78458e6db881f4e0fe2b466fe26
jason-lim <jason_lim@rp.edu.sg>
2022-08-11 08:59:28 +0800

    Updated to Gradle 7.2.1

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,9 +1,10 @@
 package com.myapplicationdev.android.tw_listview;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
